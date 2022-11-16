2f69d5de91c0d654b718aa037774b0d26d450161
Pierre <pierreterrien1@gmail.com>
2022-11-16 14:44:14 +0100

    init

---
+++ src/main/java/fr/cinpiros/Main.java
@@ -0,0 +1,15 @@
+package fr.cinpiros;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        System.out.println("Start smcTask");
+    }
+
+    @Override
+    public void onDisable() {
+        System.out.println("stop smcTask");
+    }
+}
