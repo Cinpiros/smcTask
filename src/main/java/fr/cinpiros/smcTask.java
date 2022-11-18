4ac09e417be6cf5af72116cc1a12ae783619db0a
Pierre <pierreterrien1@gmail.com>
2022-11-18 14:16:33 +0100

    add command menutask

--- src/main/java/fr/cinpiros/smcTask.java
+++ src/main/java/fr/cinpiros/smcTask.java
@@ -1,6 +1,7 @@
 package fr.cinpiros;
 
 import fr.cinpiros.commands.giveitemtest;
+import fr.cinpiros.commands.menutask;
 import fr.cinpiros.commands.test;
 import fr.cinpiros.handlers.PlayerHandler;
 import org.bukkit.Bukkit;
@@ -9,12 +10,14 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class smcTask extends JavaPlugin {
     @Override
     public void onEnable() {
-        Bukkit.getLogger().info("smcTask Enable");
 
         getCommand("test").setExecutor(new test());
         getCommand("giveitemtest").setExecutor(new giveitemtest());
+        getCommand("menutask").setExecutor(new menutask());
 
         new PlayerHandler(this);
+
+        Bukkit.getLogger().info("smcTask Enable");
     }
     @Override
     public void onDisable() {
